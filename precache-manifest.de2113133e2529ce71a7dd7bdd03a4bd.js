43d808d6809e192a1d3a1a2ad4d70d09b610c8b1
Tiago Ramalho <nehalem@gmail.com>
2019-12-11 01:16:27 -0800

    Updates

---
+++ precache-manifest.de2113133e2529ce71a7dd7bdd03a4bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99ab927f0244a77e559b2c0ef77c0734",
+    "url": "/gptquotes/index.html"
+  },
+  {
+    "revision": "6c2458f067064e5cc9ca",
+    "url": "/gptquotes/static/css/main.cd3131cb.chunk.css"
+  },
+  {
+    "revision": "b4a654d92c04eebf369e",
+    "url": "/gptquotes/static/js/2.8098a2c4.chunk.js"
+  },
+  {
+    "revision": "a1afa5f7c8dff2a316c41be2103336f5",
+    "url": "/gptquotes/static/js/2.8098a2c4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6c2458f067064e5cc9ca",
+    "url": "/gptquotes/static/js/main.82db05ed.chunk.js"
+  },
+  {
+    "revision": "25db7b4ad670281ec833",
+    "url": "/gptquotes/static/js/runtime-main.afefb18a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gptquotes/static/media/logo.5d5d9eef.svg"
+  }
+]);
